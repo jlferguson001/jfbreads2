2ffa2423ca8ab585a961a71c9caa7162ea4e5c32
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-06-24 17:21:55 -0500

    sending index and show data act 3

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -14,7 +14,13 @@ breads.get('/', (req, res) =>{
 
 //show
 breads.get('/:arrayIndex', (req, res) =>{
-    res.send(Bread[req.params.arrayIndex])
+    if (Bread[req.params.arrayIndex]) {
+        res.render('Show', {
+            bread:Bread[req.params.arrayIndex]
+        })
+    } else {
+        res.send('404')
+    }    
 })
 
 module.exports = breads
