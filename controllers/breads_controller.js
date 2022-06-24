7e85590b8f68d30b2c0ba4c2bbdfbd46a068b926
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-06-24 16:07:55 -0500

    sending index and show data act 1

---
+++ controllers/breads_controller.js
@@ -0,0 +1,16 @@
+const { application } = require('express')
+const express = require('express')
+const breads = express.Router()
+const Bread = require('../models/bread.js')
+
+//index (landing page)
+breads.get('/', (req, res) =>{
+    res.send(Bread)
+})
+
+//show
+breads.get('/:arrayIndex', (req, res) =>{
+    res.send(Bread[req.params.arrayIndex])
+})
+
+module.exports = breads
