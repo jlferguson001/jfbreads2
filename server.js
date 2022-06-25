eaf5abb4690f7b4d2a933e23282633f520849ff8
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-06-24 21:23:54 -0500

    connect to mongoose

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 // DEPENDENCIES
 const express = require('express')
 const methodOverride = require('method-override')
- 
+const mongoose = require('mongoose')
   
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
- 
+//connect to mongoose
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
+
 
 
 //middleware - two _ infront of dirname. Where issue came to crash and i couldn't figure out
